99da4e1d8c007af88d702e773f2d3b1e9edd0435
Atif Gulzar <atif.gulzar@ethosplc.com>
2020-08-03 22:43:44 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var fromEmployee = Singleton.GetInstance();
+            fromEmployee.PrintDetails("From Employee");
+
+            var fromStudent = Singleton.GetInstance();
+            fromStudent.PrintDetails("From Student");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static int _counter;
+        private static Singleton _instance;
+
+        private Singleton()
+        {
+            _counter++;
+            Console.WriteLine($"Object Creation Counter: {_counter.ToString()}");
+        }
+
+
+        public static Singleton GetInstance()
+        {
+            return _instance ??= new Singleton();
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
